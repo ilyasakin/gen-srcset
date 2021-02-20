be550880bfc5f483c55e13db5049b5bd3524902e
İlyas Akın <ashnwor@gmail.com>
2021-02-21 01:55:29 +0300

    Add commander

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello, gen-srcset');
+import { program } from 'commander';
+
+program.version('0.0.1');
+program.parse(process.argv);
+
+const main = async () => {
+  console.log('Hello, gen-srcset');
+};
+
+main();
