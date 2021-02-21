3dbe18fc63fe099a7754f1445ac898006054fe96
İlyas Akın <ashnwor@gmail.com>
2021-02-21 21:06:22 +0300

    Add initial implementation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import { program } from 'commander';
+import sharp from 'sharp';
+import path from 'path';
 
 program
   .version('0.0.1')
-  .option('-i, --input', 'Input image path')
-  .option('-b, --breakpoints', 'breakpoints which images will be generated');
+  .option('-i, --input <path>', 'Input image path')
+  .option('-b, --breakpoints <breakpoints>', 'breakpoints which images will be generated');
 
 program.parse(process.argv);
 
 const main = async () => {
-  console.log('Hello, gen-srcset');
+  const options = program.opts();
+  if (!options.breakpoints) throw new Error('breakpoints argument is required');
+  console.log(options);
+  const breakpoints = options.breakpoints.split(',').map((value) => value.replace(' ', ''));
+
+  breakpoints.forEach((breakpoint) => {
+    sharp(path.resolve(options.input))
+      .resize(parseInt(breakpoint, 10))
+      .avif()
+      .toFile(`output_${breakpoint}.avif`);
+  });
 };
 
-main();
+try {
+  main();
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
