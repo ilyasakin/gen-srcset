075a1984386d2de7149cde9227508e488df1d937
İlyas Akın <ilyas.akin@dogostore.com>
2021-02-28 18:32:18 +0300

    Bump version up to 0.1.0

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import getInputExtension from './helpers/getInputExtension';
 import getFilenameBase from './helpers/getFilenameBase';
 
 program
-  .version('0.0.1-2')
+  .version('0.1.0')
   .option('-i, --input <path>', 'Input image path')
   .option('-b, --breakpoints <breakpoints>', 'breakpoints which images will be generated')
   .option('-o, --output <output>', 'output path');
